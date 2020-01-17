62a4bc122c33472372d765931a2af105d5c887e4
DLBPointon <damonlbp@hotmail.co.uk>
2020-01-17 09:42:42 +0000

    Restrict script over running

--- cleandatav2.py
+++ cleandatav2.py
@@ -254,7 +254,7 @@ def main():
                     print('Pep splits at 2000 per file')
                 entryfunction(org, option.s, option.t, option.d, 2000)
 
-            else:
+            if option.t == 'cds' or 'ncrna':
                 if option.d:
                     print('CDs and ncRNA split at 3000 entries per file')
                 entryfunction(org, option.s, option.t, option.d, 3000)
